704e3cbaa5c8d2bf4b250a31d05597591108c2b3
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2022-02-04 21:08:12 +0100

    Add first styles to Footer component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="footer">
-        <h2 class="footer__title">Footer</h2>
+        <h2 class="footer__title">By Laura Arcidiacono - 2022</h2>
     </section>
 
 </template>
@@ -14,5 +14,9 @@ export default defineComponent({
 </script>
 
 <style scoped lang='scss'>
-
+.footer {
+    width: 100%;
+    background-color: rgb(246, 203, 60);
+    text-align: center;
+}
 </style>
