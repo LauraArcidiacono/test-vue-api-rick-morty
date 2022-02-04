de69e863cc9ba5ac3d4e061db7722e84b22d438c
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2022-02-04 13:16:24 +0100

    Set scripts on CharacterCard component
    Add props and setup  to be able to render the data of each character from API

--- src/components/CharacterCard.vue
+++ src/components/CharacterCard.vue
@@ -1,18 +1,49 @@
 <template>
     <section class="characterCard">
-        <h2 class="characterCard__title">CharacterCard</h2>
+        <div class="characterCard__image">
+            <img :src="characterImage" alt="Image of the character" class="image__image">
+        </div>
+        <h2 class="characterCard__character">{{name}}</h2>
+        <p class="character__status">{{status}}</p>
+        <p class="character__species">{{species}}</p>
+        <h4 class="characterCard_info">Last known location</h4>
+        <h3 class="info_item">{{location.name}}</h3>
+        <h4 class="characterCard_info">First seen in:</h4>
+        <h3 class="info_item">{{episode}}</h3>
     </section>
 
 </template>
 
 <script lang='ts'>
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 
 export default defineComponent({
+  name: 'CharacterCard',
+  props: ['characterId', 'characterName', 'characterStatus', 'characterSpecies', 'characterLocation', 'characterEpisode'],
+  setup(props) {
+    const characterId = ref(props.characterId);
+    const name = ref(props.characterName);
+    const status = ref(props.characterStatus);
+    const species = ref(props.characterSpecies);
+    const location = ref(props.characterLocation);
+    const episode = ref(props.characterEpisode);
 
+    const characterImage = ref(`https://rickandmortyapi.com/api/character/avatar/${characterId.value}.jpeg`);
+
+    return {
+      characterImage,
+      name,
+      status,
+      species,
+      location,
+      episode
+    };
+  }
 });
 </script>
 
 <style scoped lang='scss'>
-
+.characterCard {
+    border: solid 0.1rem black
+}
 </style>
