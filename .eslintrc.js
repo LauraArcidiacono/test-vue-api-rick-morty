649d4ba2d5c92bb2f8ea932dd13b98010411e21d
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2022-02-04 10:26:09 +0100

    Set workspace

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,10 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'comma-dangle': [2, 'never'],
+    'linebreak-style': 0,
+    'global-require': 0,
+    'eslint linebreak-style': [0, 'error', 'windows']
   },
   overrides: [
     {
