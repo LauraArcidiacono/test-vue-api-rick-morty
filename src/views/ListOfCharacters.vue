07405affab31f5fb50fd71e3ee8c23a3eb3d2bd2
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2022-02-04 10:28:20 +0100

    Create first view
    Create ListOfCharacters

---
+++ src/views/ListOfCharacters.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="listOfCharacters">
+        <h2 class="listOfCharacters__title">Dashboard</h2>
+        <CharacterCard />
+    </section>
+
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue';
+import CharacterCard from '@/components/CharacterCard.vue';
+
+export default defineComponent({
+  name: 'ListOfCharacters',
+  components: {
+    CharacterCard
+  }
+});
+</script>
+
+<style scoped lang='scss'>
+
+</style>
