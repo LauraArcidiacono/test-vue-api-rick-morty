4af34aff5902faef521f18a060ee68a64dd63e70
Maria Laura Arcidiacono <mlaura.arcidiacono@gmail.com>
2022-02-05 15:17:50 +0100

    Add image and styles to Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,14 @@
 <template>
     <section class="header">
         <h1 class="header__title">Rick and Morty characters</h1>
+        <div class="header__image">
+            <img
+                src="../assets/header1.webp"
+                alt="Image of Rick, Morty and other characters"
+                class="image__image"
+            />
+        </div>
+
     </section>
 
 </template>
@@ -16,7 +24,15 @@ export default defineComponent({
 <style scoped lang='scss'>
 .header {
     width: 100%;
-    background-color: rgb(246, 203, 60);
-    text-align: center;
+}
+h1 {
+    display: none;
+}
+.header__image {
+    width: 100%;
+    font-size: 0;
+}
+.image__image {
+    width: 100%;
 }
 </style>
